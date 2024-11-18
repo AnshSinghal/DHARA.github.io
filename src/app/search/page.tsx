88c1710c62c6eeef401dd97d7a828b2204da047a
LakshyaSingh354 <lakshya.singh354@gmail.com>
2024-11-18 10:25:47 +0530

    ngrok

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -35,7 +35,7 @@ export default function SearchPage() {
       formData.append('file', selectedFile);
 
       try {
-        const response = await fetch('https://ab4f-34-82-36-131.ngrok-free.app/upload-file', {
+        const response = await fetch('https://1feb-34-82-36-131.ngrok-free.app/upload-file', {
           method: 'POST',
           body: formData, // Send the file to the backend
         });
@@ -63,7 +63,7 @@ export default function SearchPage() {
 
     try {
       console.log("query", query);
-      const response = await fetch("https://ab4f-34-82-36-131.ngrok-free.app/query", {
+      const response = await fetch("https://1feb-34-82-36-131.ngrok-free.app/query", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
